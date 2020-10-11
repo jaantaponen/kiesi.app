897ce601840d1dd287ede65d03489d44142b1eca
Otto Otsamo <ootsamo@gmail.com>
2020-10-11 05:03:29 +0300

    Refactor the sidebar into an ascended state of consciousness

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,8 @@ import {BrowserRouter, Redirect, Route, Switch, useLocation, useRouteMatch} from
 import Map from './Map';
 import Login from './Login';
 import PoolTool from './PoolTool';
+import Sidebar from './Sidebar';
+import MenuButton from './MenuButton';
 
 
 const tokenInStorage = () => {
@@ -11,33 +13,47 @@ const tokenInStorage = () => {
 };
 function App() {
   const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+  const [menuVisible, setMenuVisible] = useState<boolean>(false);
   const match = useLocation();
+
   useEffect(() => {
     if (tokenInStorage()) setLoggedIn(true);
     else setLoggedIn(false);
   }, [match])
+
   useEffect(() => {
     console.log(match);
   }, [match, isLoggedIn])
+
+  const onMenuClick = () => {
+    setMenuVisible(!menuVisible);
+  }
+
   return (
     <div className="App">
         <Switch>
-          <Route path="/" exact render={() => {
-            if (tokenInStorage()) {
-              return <Map />
-            } else {
-              return <Redirect to="/login" />
-            }
-          }}/>
-          <Route path="/tool" exact render={() => {
-            if (tokenInStorage()) {
-              return <PoolTool />
-            } else {
-              return <Redirect to="/login" />
-            }
-          }}/>
-          <Route path="/login"  component={Login}/>
-        </Switch>
+        <Route path="/login" component={Login}/>
+        <div>
+          <div id="container" className={menuVisible ? "container-menu-visible" : ""}>
+            <MenuButton onClick={onMenuClick} />
+            <Route path="/" exact render={() => {
+              if (tokenInStorage()) {
+                return <Map />
+              } else {
+                return <Redirect to="/login" />
+              }
+            }}/>
+            <Route path="/tool" exact render={() => {
+              if (tokenInStorage()) {
+                return <PoolTool />
+              } else {
+                return <Redirect to="/login" />
+              }
+            }}/>
+          </div>
+          <Sidebar />
+        </div>
+      </Switch>
     </div>
   );
 }
