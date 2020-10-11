c4ffe635ffda078763b50910a64200df62491726
JaanTaponen <jaan.taponen@hotmail.com>
2020-10-11 08:55:04 +0300

    infopage

---
+++ infopage/src/components/layout/partials/FooterNav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+
+const FooterNav = ({
+  className,
+  ...props
+}) => {
+
+  const classes = classNames(
+    'footer-nav',
+    className
+  );
+
+  return (
+<div></div>
+  );
+}
+
+export default FooterNav;
