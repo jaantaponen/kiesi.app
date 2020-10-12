590daadb7940bdd3077d4caea0316bf1940e236c
Kääriäinen Santeri <kaarias3@kosh.org.aalto.fi>
2020-10-12 16:20:55 +0300

    add cta

--- infopage/src/components/sections/Cta.js
+++ infopage/src/components/sections/Cta.js
@@ -57,11 +57,11 @@ const Cta = ({
               </h3>
           </div>
           <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your best email">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+            <a href="mailto:jaan.taponen@aalto.fi">
+              <h3 className="m-0">
+                Contact us!
+              </h3>
+            </a>
           </div>
         </div>
       </div>
